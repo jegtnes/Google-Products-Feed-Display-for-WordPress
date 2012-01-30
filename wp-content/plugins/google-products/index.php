4032f47076c5d83bc7c90530915f0999ed08935a
Alexander Jegtnes <saccosekk@gmail.com>
2012-01-30 17:53:13 +0000

    Added plugin index file

---
+++ wp-content/plugins/google-products/index.php
@@ -0,0 +1,10 @@
+<?php  
+    /* 
+    Plugin Name: Google Products feed syndicator 
+    Plugin URI: https://github.com/jegtnes/Google-Products-Feed-Syndicator-for-WordPress
+    Description: A plugin to acquire, parse and display products from a Google Products feed.
+    Author: Alexander Jegtnes 
+    Version: 0.1
+    Author URI: http://jegtnes.co.uk
+    */  
+?>
