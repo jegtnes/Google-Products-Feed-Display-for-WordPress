7c2456cec08756b658444405ca3c91a728dfeb23
Alexander Jegtnes <saccosekk@gmail.com>
2012-02-07 17:26:13 +0000

    Added non-working options page

--- wp-content/plugins/google-products/index.php
+++ wp-content/plugins/google-products/index.php
@@ -1,10 +1,20 @@
 <?php  
     /* 
-    Plugin Name: Google Products feed syndicator 
-    Plugin URI: https://github.com/jegtnes/Google-Products-Feed-Syndicator-for-WordPress
+    Plugin Name: Google Products Feed Display 
+    Plugin URI: https://github.com/jegtnes/Google-Products-Feed-Display-for-WordPress
     Description: A plugin to acquire, parse and display products from a Google Products feed.
     Author: Alexander Jegtnes 
     Version: 0.1
     Author URI: http://jegtnes.co.uk
     */  
+
+    function oscimp_admin() {  
+        include('goopro_admin.php');  
+    }  
+
+    function goopro_admin_init() {  
+        add_options_page("Google Products Feed Display: Settings", "Google Products Feed Display", 1, "google_products_feed_display", "goopro_admin");
+    }  
+
+    add_action('admin_menu', 'goopro_admin_init');  
 ?>
